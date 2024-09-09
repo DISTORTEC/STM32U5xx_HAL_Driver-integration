919afc2ff8fa5ca67e573d88cd28e1b270bfe78e
Kamil Szczygiel <distortec@distortec.com>
2024-09-09 16:30:53 +0200

    Add wrapper for original stm32u5xx.h

---
+++ include/stm32u5xx.h
@@ -0,0 +1,18 @@
+/**
+ * \file
+ * \brief Wrapper for original stm32u5xx.h
+ *
+ * \author Copyright (C) 2021-2024 Kamil Szczygiel https://distortec.com https://freddiechopin.info
+ *
+ * \par License
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDE_STM32U5XX_H_
+#define INCLUDE_STM32U5XX_H_
+
+#include "distortos/chip/CMSIS-proxy.h"
+#include_next "stm32u5xx.h"
+
+#endif	/* INCLUDE_STM32U5XX_H_ */
